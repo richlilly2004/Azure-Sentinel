37857b673bb21c013c9b3196daf508cb8df2eb00
richlilly2004 <rich.lilly@outlook.com>
2020-04-14 07:00:11 -0500

    Lighthouse initial commits

---
+++ Lighthouse/CustomerOnboard.ps1
@@ -0,0 +1,3 @@
+﻿##Run in Cloud Shell on customer's tenant
+$location = "eastus"
+New-AzDeployment -Location $location -DeploymentName "SentinelaaS" -TemplateFile "./rgDelegatedResourceManagement.json" -TemplateParameterFile "./Netrix Prod B2B - Azure Sentinel MSSP.json"
