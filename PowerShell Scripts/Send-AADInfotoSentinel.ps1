af53c9e7fe6f117091ce1cb664a1b8baa97191e4
richlilly2004 <rich.lilly@outlook.com>
2020-04-16 06:20:02 -0500

    Create AAD Devices

---
+++ PowerShell Scripts/Send-AADInfotoSentinel.ps1
@@ -0,0 +1,18 @@
+﻿#Required Variables
+$CustomerID = Get-AutomationVariable -Name 'Sentinel_CustomerID'
+$SharedKey = Get-AutomationVariable -Name 'Sentinel_SharedKey'
+$userlogType = 'AADUsers'
+$devicelogType = 'AADDevices'
+$TenantID = Get-AutomationVariable -Name 'TenantID'
+$MyCredential = Get-AutomationPSCredential -Name 'AAD Account'
+
+#Connect to Azure AD
+Connect-AzureAD -TenantId $TenantID -Credential $MyCredential
+
+#Collecting devices from AzureAD
+$DevicesCollected = Get-AzureADDevice -All $True
+
+# Ingesting data all at the same time
+$JSONDevice = ConvertTo-Json $DevicesCollected -Compress -ErrorAction Stop 
+$TimeStampfield = Get-date
+Send-OMSAPIIngestionFile -customerId $customerId -sharedKey $sharedKey -body $JSONDevice -logType $devicelogType -TimeStampField $Timestampfield -Verbose
